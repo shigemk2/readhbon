3e7a6903fdd7ca030817377e68401b8fd9463b0c
shigemk2 <i.am.shige@gmail.com>
2015-08-05 20:32:42 +0900

    畳み込みを使ったアプリカティブ評価

---
+++ sequencefoldA.hs
@@ -0,0 +1,16 @@
+import Control.Applicative
+
+sequenceA :: (Applicative f) => [f a] -> f [a]
+sequenceA = foldr (liftA2 (:)) (pure [])
+
+main = do
+    print $ sequenceA [Just 1, Just 2]
+    print $ sequenceA [Just 3, Just 2, Just 1]
+    print $ sequenceA [Just 3, Nothing, Just 1]
+    print $ sequenceA [[1,2,3],[4,5,6]]
+    print $ sequenceA [[1,2,3],[4,5,6],[3,4,4],[]]
+    -- equal
+    print $ (:) <$> Just 1 <*> sequenceA [Just 2]
+    print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> sequenceA [])
+    print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> Just [])
+    print $ (:) <$> Just 1 <*> Just [2]
