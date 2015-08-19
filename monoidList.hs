d086b2c3d227e68ad5c8b4391bda264d5368be97
shigemk2 <i.am.shige@gmail.com>
2015-08-19 20:40:18 +0900

    リストはモノイド

---
+++ monoidList.hs
@@ -0,0 +1,11 @@
+import Data.Monoid
+
+-- instance Monoid [a] where
+--     mempty = []
+--     mappend = (++)
+
+main = do
+    print $ [1,2,3] `mappend` [4,5,6]
+    print $ ("one" `mappend` "two") `mappend` "tree"
+    print $ "one" `mappend` ("two" `mappend` "tree")
+    print $ "one" `mappend` "two" `mappend` "tree"
