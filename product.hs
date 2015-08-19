24006694d5af26052f5d1f91024d744b3d6ec751
shigemk2 <i.am.shige@gmail.com>
2015-08-19 20:52:10 +0900

    Fix Product and Sum

--- product.hs
+++ product.hs
@@ -1,13 +1,16 @@
 import Data.Monoid
 
+-- getProductなどでモノイドの中身の値を取り出す
 main = do
     -- product
+    print $ Product 3 `mappend` Product 9
     print $ getProduct $ Product 3 `mappend` Product 9
     print $ getProduct $ Product 3 `mappend` mempty
     print $ getProduct $ Product 3 `mappend` Product 4 `mappend` Product 2
     print $ getProduct $ Product 3 `mappend` (Product 4 `mappend` Product 2)
     print $ getProduct . mconcat . map Product $ [3,4,2]
     -- sum
+    print $ Sum 2 `mappend` Sum 9
     print $ getSum $ Sum 2 `mappend` Sum 9
     print $ getSum $ Sum 2 `mappend` Sum 9 `mappend` Sum 9
     print $ getSum $ Sum 2 `mappend` (Sum 9 `mappend` Sum 9)
