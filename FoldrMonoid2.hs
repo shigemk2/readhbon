ac49500b5e4ca198ba01f77e5e4c4628a6f6a04a
shigemk2 <i.am.shige@gmail.com>
2015-08-20 16:04:55 +0900

    foldr

---
+++ FoldrMonoid2.hs
@@ -0,0 +1,27 @@
+import Data.Monoid
+import qualified Data.Foldable as F
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x `mappend`
+                             F.foldMap f r
+
+testTree = Node 5
+           (Node 3
+               (Node 1 EmptyTree EmptyTree)
+               (Node 6 EmptyTree EmptyTree)
+           )
+           (Node 9
+               (Node 8 EmptyTree EmptyTree)
+               (Node 10 EmptyTree EmptyTree)
+           )
+
+main = do
+    print $ F.foldr (+) 0 testTree
+    print $ F.foldl (*) 1 testTree
+    print $ getAny $ F.foldMap ( \ x -> Any $ x == 3) testTree
+    print $ getAny $ F.foldMap ( \ x -> Any $ x > 15) testTree
+    print $ F.foldMap ( \ x -> [x]) testTree
