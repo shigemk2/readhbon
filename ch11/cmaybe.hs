4e140fad86b3c9991bdfec2464e8318975f0421a
shigemk2 <i.am.shige@gmail.com>
2015-09-04 10:48:47 +0900

    ファンクター則を満たしていないFunctorのインスタンスをつくる

--- ch11/cmaybe.hs
+++ ch11/cmaybe.hs
@@ -1,6 +1,17 @@
 data CMaybe a = CNothing | CJust Int a deriving (Show)
+-- CMaybeをFunctorのインスタンスにしてみる
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter+1) (f x)
 
 main = do
     -- print $ CNothing
     print $ CJust 0 "haha"
     print $ CJust 100 [1,2,3]
+    print $ fmap (++"ha") (CJust 0 "ho")
+    print $ fmap (++"he") (fmap (++"ha") (CJust 0 "ho"))
+    print $ fmap (++"blah") CNothing
+    -- CMaybeがファンクター則を満たしていない反例
+    -- id = fmap idではないので、ファンクターではない。
+    print $ fmap id (CJust 0 "haha") -- CJust 1 "haha"
+    print $ id (CJust 0 "haha") -- CJust 0 "haha"
