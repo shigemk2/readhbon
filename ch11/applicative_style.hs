0f2900943446d4ec670b6f75ed8cc0079af6bbf7
shigemk2 <i.am.shige@gmail.com>
2015-09-05 00:42:51 +0900

    アプリ化ティブスタイル

--- ch11/applicative_style.hs
+++ ch11/applicative_style.hs
@@ -4,3 +4,6 @@ main = do
     print $ pure (+) <*> Just 3 <*> Just 5
     print $ pure (+) <*> Just 3 <*> Nothing
     print $ pure (+) <*> Nothing <*> Just 5
+
+    print $ (++) <$> Just "johntra" <*> Just "volta"
+    print $ (++) "johntra" "volta"
