fd0da5f1eed95b1661d94417d7e426bdbea10d81
shigemk2 <i.am.shige@gmail.com>
2015-09-06 10:08:55 +0900

    liftA2による持ち上げの例 その2

--- ch11/lifta2.hs
+++ ch11/lifta2.hs
@@ -3,7 +3,10 @@ import Control.Applicative
 main = do
     print $ fmap (\x -> [x]) (Just 4)
     print $ 3 : [4]
+    print $ [1,2,3] ++ [4]
     -- Just 3 と Just [4] があります。ここから Just [3,4]を作る
     -- 持ち上げ
     print $ liftA2 (:) (Just 3) (Just [4])
     print $ (:) <$> Just 3 <*> Just [4]
+    print $ liftA2 (++) (Just [1,2]) (Just [3,4])
+    print $ (++) <$> Just [1,2] <*> Just [3,4]
