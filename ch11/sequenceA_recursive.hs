fc4bdc838d24a5619dc75151015d1236479e7205
shigemk2 <i.am.shige@gmail.com>
2015-09-06 10:33:03 +0900

    再帰でsequenceA

---
+++ ch11/sequenceA_recursive.hs
@@ -0,0 +1,14 @@
+import Control.Applicative
+
+sequenceA :: (Applicative f) => [f a] => f [a]
+sequenceA [] = pure []
+sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+
+main = do
+    print $ sequenceA [Just 1, Just 2]
+    -- print $ (:) <$> Just 1 <*> sequenceA [Just 2, Just 3]
+    print $ (:) <$> Just 1 <*> sequenceA [Just 2]
+    print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> sequenceA [])
+    print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> Just [])
+    print $ (:) <$> Just 1 <*> Just [2]
+
