a7b869b22e496a9bdc2b86f49b7ec8e28bbaaff8
shigemk2 <i.am.shige@gmail.com>
2015-09-06 10:33:59 +0900

    もとのsequence

--- ch11/sequenceA_recursive.hs
+++ ch11/sequenceA_recursive.hs
@@ -5,6 +5,7 @@ sequenceA [] = pure []
 sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
 
 main = do
+    print $ sequence [Just 1, Just 2, Just 3]
     print $ sequenceA [Just 1, Just 2]
     -- print $ (:) <$> Just 1 <*> sequenceA [Just 2, Just 3]
     print $ (:) <$> Just 1 <*> sequenceA [Just 2]
