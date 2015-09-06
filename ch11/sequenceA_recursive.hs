6b2feb50c8d5bfc8de225cfdfdbd89cb671058c6
shigemk2 <i.am.shige@gmail.com>
2015-09-06 10:44:28 +0900

    Nothingとか[]が一つでも入っていたときの挙動

--- ch11/sequenceA_recursive.hs
+++ ch11/sequenceA_recursive.hs
@@ -12,4 +12,11 @@ main = do
     print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> sequenceA [])
     print $ (:) <$> Just 1 <*> ((:) <$> Just 2 <*> Just [])
     print $ (:) <$> Just 1 <*> Just [2]
+    print $ sequenceA [Just 3, Just 2, Just 1]
+    print $ sequenceA [Just 3, Nothing, Just 1]
+    print $ sequenceA [(+3), (+2), (+1)] 3
+    print $ sequenceA [[1,2,3], [4,5,6]]
+    print $ sequenceA [[1,2,3], [], [4,5,6]]
+    print $ sequenceA [[1,2,3], [4,5,6], [3,4,4], []]
+
 
