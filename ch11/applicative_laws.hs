15fe95f540f6690897588751328798b5403237ea
shigemk2 <i.am.shige@gmail.com>
2015-09-06 09:50:21 +0900

    pure f <*> pure x = pure (f x)

--- ch11/applicative_laws.hs
+++ ch11/applicative_laws.hs
@@ -7,3 +7,6 @@ main = do
     print $ (pure id <*> Just 3) == Just 3
     -- pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
     -- print $ pure (.) <*> [1,2,3] <*> [1,2,3] <*> [1,2,3]
+    -- pure f <*> pure x = pure (f x)
+    print $ pure (+3) <*> Just 3
+    -- print $ pure ((+3) $ Just 3)
