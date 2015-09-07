246556bd41bec9164ef9f0e8294325cd35741ef6
shigemk2 <i.am.shige@gmail.com>
2015-09-07 17:27:48 +0900

    適当にアプリカティブ則をHaskellで試す

--- ch11/applicative_laws.hs
+++ ch11/applicative_laws.hs
@@ -2,13 +2,14 @@ import Control.Applicative
 
 main = do
     -- pure id <*> v = v
-    print $ pure id <*> Just 3
-    print $ Just 3
-    print $ (pure id <*> Just 3) == Just 3
+    print $ pure id <*> [1,2,3]
+    print $ [1,2,3]
+    print $ (pure id <*> [1,2,3]) == [1,2,3]
     -- pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
     -- print $ pure (.) <*> [1,2,3] <*> [1,2,3] <*> [1,2,3]
     -- pure f <*> pure x = pure (f x)
-    print $ pure (+3) <*> Just 3
+    pure sum <*> pure [1,2,3]
+    print $ pure (sum [1,2,3])
     -- print $ pure ((+3) $ Just 3)
     -- u <*> pure y = pure ($ y) <*> u
     -- print $ pure $ Just 4
