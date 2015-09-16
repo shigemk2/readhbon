39c073b5812af077b0375cb6db328fe4bae769b1
shigemk2 <i.am.shige@gmail.com>
2015-09-16 20:45:49 +0900

    「落ちる」表現

--- ch13/pierre.hs
+++ ch13/pierre.hs
@@ -2,11 +2,14 @@ type Birds = Int
 type Pole = (Birds, Birds)
 
 -- 鳥
-landLeft :: Birds -> Pole -> Pole
-landLeft n (left, right) = (left + n, right)
-landRight :: Birds -> Pole -> Pole
-landRight n (left, right) = (left, right + n)
-
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise = Nothing
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise = Nothing
 -- バランス棒
 x -: f = f x
 
@@ -14,4 +17,5 @@ main = do
     print $ landLeft 2 (0, 0)
     print $ landLeft 1 (1, 2)
     print $ landRight (-1) (1, 2)
-    print $ (0, 0) -: landLeft 1 -: landRight 1 -: landLeft 2
+    print $ landRight 10 (0, 3)
+
