2ab35c3526cf8cb448b67819eb67727abc799ee5
shigemk2 <i.am.shige@gmail.com>
2015-09-19 00:40:55 +0900

    最初の計算

--- ch01/first.hs
+++ ch01/first.hs
@@ -2,3 +2,7 @@ main = do
     print $ 2 + 15
     print $ 49 + 100
     print $ 1092 - 1472
+    print $ 5 / 2
+    print $ (50 * 100) - 4999
+    print $ 50 * 100 - 4999
+    print $ 50 * (100 - 4999)
