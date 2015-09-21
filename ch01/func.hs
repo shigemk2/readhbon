75e97bf6e72bea6b01e4c3d22c589a4025a8d2b9
shigemk2 <shigemk2@shigemk2-mbp.local>
2015-09-22 03:35:54 +0900

    関数呼び出し

---
+++ ch01/func.hs
@@ -0,0 +1,8 @@
+main = do
+    print $ min 9 10
+    print $ min 3.4 3.2
+    print $ max 100 101
+    print $ succ 9 + max 5 4 + 1
+    print $ (succ 9) + (max 5 4) + 1
+    print $ succ 9 * 10
+    print $ div 92 10
