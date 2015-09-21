0142ddcdddf8cddd9a25826d2d451029abdf7b80
shigemk2 <shigemk2@shigemk2-mbp.local>
2015-09-22 05:16:54 +0900

    関数作成

--- ch01/double.hs
+++ ch01/double.hs
@@ -1,5 +1,9 @@
 doubleMe x = x + x
+doubleUs x y = x * 2 + y * 2
 
 main = do
     print $ doubleMe 9
     print $ doubleMe 8.3
+    print $ doubleUs 4 9
+    print $ doubleUs 2.3 34.2
+    print $ doubleUs 28 88 + doubleMe 123
