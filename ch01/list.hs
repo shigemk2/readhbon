ecaf19dc34668b2458333c8f9f241b52d2893a73
shigemk2 <i.am.shige@gmail.com>
2015-09-22 12:06:40 +0900

    コメントアウト追加

--- ch01/list.hs
+++ ch01/list.hs
@@ -1,6 +1,7 @@
 main = do
     let lostNumbers = [4,8,15,16,23,42]
     print $ lostNumbers
+    -- ++は最後まで走査するので遅い
     print $ [1,2,3,4] ++ [9,10,11,12]
     print $ "hello" ++ " " ++ "world"
     print $ ['w','o'] ++ ['o','t']
