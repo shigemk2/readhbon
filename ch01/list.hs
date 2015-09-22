faa49896f99b25a8a529a1f6ed9c289fe86f6dec
shigemk2 <i.am.shige@gmail.com>
2015-09-22 12:02:26 +0900

    リストの結合

--- ch01/list.hs
+++ ch01/list.hs
@@ -1,3 +1,6 @@
 main = do
     let lostNumbers = [4,8,15,16,23,42]
     print $ lostNumbers
+    print $ [1,2,3,4] ++ [9,10,11,12]
+    print $ "hello" ++ " " ++ "world"
+    print $ ['w','o'] ++ ['o','t']
