790b5faf20ff0c4ac7b6fe5ec8ec8f93ecd505ad
shigemk2 <i.am.shige@gmail.com>
2015-09-22 11:38:56 +0900

    関数呼び出し

--- ch01/double.hs
+++ ch01/double.hs
@@ -1,5 +1,7 @@
 doubleMe x = x + x
 doubleUs x y = x * 2 + y * 2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
 
 main = do
     print $ doubleMe 9
@@ -7,3 +9,6 @@ main = do
     print $ doubleUs 4 9
     print $ doubleUs 2.3 34.2
     print $ doubleUs 28 88 + doubleMe 123
+    print $ doubleSmallNumber' 100
+    -- こういう書き方もできちゃう
+    print $ conanO'Brien
