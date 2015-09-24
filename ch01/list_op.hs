67ec9d7b79cf3650400d08c614a514d51df09b90
shigemk2 <i.am.shige@gmail.com>
2015-09-24 16:22:46 +0900

    リスト

--- ch01/list_op.hs
+++ ch01/list_op.hs
@@ -5,3 +5,8 @@ main = do
     print $ init [5,4,3,2,1]
     -- print $ head []
     -- print $ tail []
+    -- リストの長さを測る
+    print $ length [1,2,3,4,5]
+    -- リストが空かどうか判定する
+    print $ null [1,2,3]
+    print $ null []
