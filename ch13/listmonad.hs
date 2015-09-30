1d13b7988629f5d4a15731adf65d8f1f874a1ef7
shigemk2 <i.am.shige@gmail.com>
2015-09-30 20:59:29 +0900

    場合分けの木構造

--- ch13/listmonad.hs
+++ ch13/listmonad.hs
@@ -3,3 +3,5 @@ main = do
     print $ [3,4,5] >>= \x -> [x,-x]
     print $ [] >>= \x -> ["bad", "mad", "rad"]
     -- [1,2,3] >>= \x -> []
+    -- 場合分けの木構造
+    print $ [1,2] >>= \n -> ['a','b'] >>= \ch -> return (n, ch)
