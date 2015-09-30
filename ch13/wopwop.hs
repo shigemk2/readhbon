c5d371471c7a49cf5843bdc7b6da61e2633cddd1
shigemk2 <i.am.shige@gmail.com>
2015-09-30 20:55:55 +0900

    wopwop

--- ch13/wopwop.hs
+++ ch13/wopwop.hs
@@ -1,7 +1,12 @@
+-- do 式の中でパターンマッチが失敗した場合、 Monad 型クラスの一員であるfail 関数が使われる
 wopwop :: Maybe Char
 wopwop = do
     (x:xs) <- Just ""
     return x
 
 main = do
+    print $ Just "Hello" >>= \(x:xs) -> Just x
+    -- Non-exhaustive patterns in lambda
+    -- failのデフォルト実装だと死ぬ
+    -- print $ Just "" >>= \(x:xs) -> Just x
     print $ wopwop
