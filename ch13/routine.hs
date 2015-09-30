73cf371ac87487edf07d8fb211dd641908be37d9
shigemk2 <i.am.shige@gmail.com>
2015-09-30 20:22:08 +0900

    routine

---
+++ ch13/routine.hs
@@ -0,0 +1,38 @@
+type Birds = Int
+type Pole = (Birds, Birds)
+
+-- 鳥
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise = Nothing
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise = Nothing
+
+routine :: Maybe Pole
+routine = do
+    start <- return (0, 0)
+    first <- landLeft 2 start
+    second <- landRight 2 first
+    landLeft 1 second
+
+-- doを使わないと、こんな感じで超だるい
+-- routine :: Maybe Pole
+-- routine = case landLeft 1 (0, 0) of
+--               Nothing -> Nothing
+--               Just pole1 -> case landRight 4 pole1 of
+--                                 Nothing -> Nothing
+--                                 Just pole2 -> case landLeft 2 pole2 of
+--                                                   Nothing -> Nothing
+--                                                   Just pole3 -> landLeft 1 pole3
+
+
+main = do
+    print $ landLeft 2 (0, 0)
+    print $ landLeft 1 (1, 2)
+    print $ landRight (-1) (1, 2)
+    print $ landRight 10 (0, 3)
+    print $ landRight 1 (0, 0) >>= landLeft 2
+    print $ routine
